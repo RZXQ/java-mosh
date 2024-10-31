36a68b62301cf9fe80ca740c076c1a4d39c54cca
Reacher <macryze@icloud.com>
2024-10-31 18:21:41 +0800

    Fix inconsistent indentation in _10_Constants.java
    Adjusted the indentation in the file _10_Constants.java for the main method. This change ensures consistent code formatting, enhancing readability and maintainability.

--- src/part_01/_02_Types/_10_Constants/_10_Constants.java
+++ src/part_01/_02_Types/_10_Constants/_10_Constants.java
@@ -1,10 +1,8 @@
 package part_01._02_Types._10_Constants;
 
 public class _10_Constants {
-
-	public static void main(String[] args) {
-		// Define a constant
-		final float PI = 3.14F; // PI is a constant, value cannot be changed
-	}
-
+    public static void main(String[] args) {
+        // Define a constant
+        final float PI = 3.14F; // PI is a constant, value cannot be changed
+    }
 }
