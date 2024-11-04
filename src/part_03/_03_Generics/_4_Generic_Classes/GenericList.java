4062e035ba06f27ee6b399647a798ffaa4003c47
Reacher <macryze@icloud.com>
2024-11-05 00:58:54 +0800

    Refactor generics and remove untyped List class
    Replaced the untyped List class with a type-safe GenericList class. Updated User class to remove redundant array implementation. Enhanced _4_Generic_Classes.java with comments highlighting generics' benefits.

--- src/part_03/_03_Generics/_4_Generic_Classes/GenericList.java
+++ src/part_03/_03_Generics/_4_Generic_Classes/GenericList.java
@@ -1,18 +1,15 @@
 package part_03._03_Generics._4_Generic_Classes;
 
-// T represents Type
 public class GenericList<T> {
+    private final T[] items = (T[]) new Object[10];
 
-	private T[] list = (T[]) new Object[10];
+    private int count;
 
-	private int count;
+    public void add(T item) {
+        items[count++] = item;
+    }
 
-	public void add(T item) {
-		list[count++] = item;
-	}
-
-	public T get(int index) {
-		return list[index];
-	}
-
+    public T get(int index) {
+        return items[index];
+    }
 }
--- src/part_03/_03_Generics/_4_Generic_Classes/_4_Generic_Classes.java
+++ src/part_03/_03_Generics/_4_Generic_Classes/_4_Generic_Classes.java
@@ -1,14 +1,28 @@
 package part_03._03_Generics._4_Generic_Classes;
 
 public class _4_Generic_Classes {
+    public static void main(String[] args) {
+        // 1 - Benefit of Generics #1: Type Safety
+        // Using generics enforces type constraints at compile-time, ensuring only Integer objects can be stored.
+        var list = new GenericList<Integer>();
 
-	public static void main(String[] args) {
-		// Generics provide compile-time type safety
-		// No need to explicitly cast to User from Object
-		var list = new GenericList<User>();
+        // 2 - Adding Elements
+        // This adds an Integer to the list. Generics ensure that no other types can be added accidentally.
+        list.add(1);
 
-		list.add(new User()); // Adding a User object to the list
-		User user = list.get(0); // Directly retrieving a User object without casting
-	}
+        // 3 - Benefit of Generics #2: Eliminates Casting
+        // No explicit cast is required when retrieving the value, making code cleaner and safer.
+        int number = list.get(0);
 
+        // 4 - Example with a different type
+        // Creating another GenericList, this time for storing User objects
+        var list2 = new GenericList<User>();
+
+        // 5 - Adding a User object to the list
+        list2.add(new User());
+
+        // 6 - Retrieving a User Object with Type Safety
+        // Here, we get a User object directly without casting.
+        User user = list2.get(0);
+    }
 }
