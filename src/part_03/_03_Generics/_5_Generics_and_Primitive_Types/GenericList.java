9ccd58f509626e7a371e7c5b3fe406fbe9e5a58d
Reacher <macryze@icloud.com>
2024-11-05 01:27:43 +0800

    Format `GenericList` class and make `list` final
    Aligned the indentation in the `GenericList` class to use spaces instead of tabs. Additionally, made the `list` array final to ensure it is not reassigned.

--- src/part_03/_03_Generics/_5_Generics_and_Primitive_Types/GenericList.java
+++ src/part_03/_03_Generics/_5_Generics_and_Primitive_Types/GenericList.java
@@ -1,17 +1,16 @@
 package part_03._03_Generics._5_Generics_and_Primitive_Types;
 
 public class GenericList<T> {
+    private final T[] list = (T[]) new Object[10];
 
-	private T[] list = (T[]) new Object[10];
+    private int count;
 
-	private int count;
+    public void add(T item) {
+        list[count++] = item;
+    }
 
-	public void add(T item) {
-		list[count++] = item;
-	}
-
-	public T get(int index) {
-		return list[index];
-	}
+    public T get(int index) {
+        return list[index];
+    }
 
 }
