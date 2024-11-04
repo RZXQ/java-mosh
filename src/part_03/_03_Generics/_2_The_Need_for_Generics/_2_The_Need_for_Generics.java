58d2af907327c4a21741b02063179f9537e68be0
Reacher <macryze@icloud.com>
2024-11-05 00:12:33 +0800

    Add detailed comments to illustrate the need for generics
    Enhanced the _2_The_Need_for_Generics class by adding comments that explain the usefulness of generics in Java. These comments clarify how generics allow for the creation of classes that can handle multiple data types, thus avoiding redundant code.

--- src/part_03/_03_Generics/_2_The_Need_for_Generics/_2_The_Need_for_Generics.java
+++ src/part_03/_03_Generics/_2_The_Need_for_Generics/_2_The_Need_for_Generics.java
@@ -1,10 +1,13 @@
 package part_03._03_Generics._2_The_Need_for_Generics;
 
+// This class illustrates why generics are useful in Java.
+// Without generics, we need separate classes for each data type (e.g., List for integers, UserList for User objects).
+// Generics allow us to create a single class that can work with different data types.
 public class _2_The_Need_for_Generics {
-
-	public static void main(String[] args) {
-		var list = new List();
-		list.add(1);
-	}
-
+    public static void main(String[] args) {
+        // Example: Using a non-generic List class, only integers can be added.
+        // With generics, we could define a single List<T> that works with any data type.
+        var list = new List();
+        list.add(1); // Works for integers, but not flexible for other data types.
+    }
 }
