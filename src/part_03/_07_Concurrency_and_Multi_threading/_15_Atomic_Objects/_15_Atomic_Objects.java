6625331c3b3cab413c8b21f02d6cadc18d303099
Reacher <macryze@icloud.com>
2024-11-05 12:22:31 +0800

    Add DownloadFileTask with atomic increment
    Implemented DownloadFileTask that uses DownloadStatus to increment total bytes atomically. Added ThreadsDemo to demonstrate concurrent execution of DownloadFileTask using multiple threads. Ensured thread-safe operations with AtomicInteger in DownloadStatus.

---
+++ src/part_03/_07_Concurrency_and_Multi_threading/_15_Atomic_Objects/_15_Atomic_Objects.java
@@ -0,0 +1,7 @@
+package part_03._07_Concurrency_and_Multi_threading._15_Atomic_Objects;
+
+public class _15_Atomic_Objects {
+    public static void main(String[] args) {
+        ThreadsDemo.show();
+    }
+}
