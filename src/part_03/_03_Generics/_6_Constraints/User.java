fbdc25a49b91f846e09d03d674fc28d814bc3f81
Reacher <macryze@icloud.com>
2024-11-11 02:25:28 +0800

    Refactor User class to use raw Comparable type
    Updated the User class to implement raw Comparable instead of Comparable<User>. Modified the compareTo method to accept an Object parameter. Also, added detailed comments explaining the usage of GenericList with constraints in the example code.

--- src/part_03/_03_Generics/_6_Constraints/User.java
+++ src/part_03/_03_Generics/_6_Constraints/User.java
@@ -2,9 +2,11 @@ package part_03._03_Generics._6_Constraints;
 
 import org.jetbrains.annotations.NotNull;
 
-public class User implements Comparable<User>, Cloneable {
-    @Override
-    public int compareTo(@NotNull User o) {
-        return 0;
-    }
+public class User implements Comparable, Cloneable {
+
+	@Override
+	public int compareTo(@NotNull Object o) {
+		return 0;
+	}
+
 }
--- src/part_03/_03_Generics/_6_Constraints/_6_Constraints.java
+++ src/part_03/_03_Generics/_6_Constraints/_6_Constraints.java
@@ -1,7 +1,18 @@
 package part_03._03_Generics._6_Constraints;
 
 public class _6_Constraints {
-    public static void main(String[] args) {
-        GenericList<User> users = new GenericList<>();
-    }
+
+	public static void main(String[] args) {
+		// 1 - Example: Using Constraints in Generics
+		// - Uncomment the line below to create a `GenericList` restricted to
+		// types that are instances of `Number` or its subclasses (e.g., Integer, Float).
+		// var list = new GenericList<Number>();
+
+		// 2 - Using `User` in a `GenericList`
+		// - Here, we create a `GenericList` that accepts `User` objects because
+		// `User` implements both `Comparable` and `Cloneable` interfaces, which
+		// are the constraints specified in `GenericList`.
+		new GenericList<User>();
+	}
+
 }
