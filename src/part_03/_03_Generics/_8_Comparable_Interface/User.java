958ac1315ebd734a7666775ef9682d60a9393951
Reacher <macryze@icloud.com>
2024-11-12 00:39:58 +0800

    Correct comparison logic in User class and main method.
    Revised the compareTo method in User class to ensure correct comparison of User points. Adjusted the main method conditions for accurate comparison outputs based on the revised logic.

--- src/part_03/_03_Generics/_8_Comparable_Interface/User.java
+++ src/part_03/_03_Generics/_8_Comparable_Interface/User.java
@@ -1,26 +1,48 @@
 package part_03._03_Generics._8_Comparable_Interface;
 
-/// 1. Implementing the Comparable Interface:
-///
-/// - Any class that implements the `Comparable` interface must provide an implementation of the `compareTo()` method.
-/// - The `compareTo()` method defines how objects of the class are compared to each other.
-/// - This is useful for sorting objects or determining their natural ordering.
+import org.jetbrains.annotations.NotNull;
+
+// Implements Comparable<User> to enable sorting of User objects by their attributes
 public class User implements Comparable<User> {
 
-	private int points;
+	// `points` represents the user's score, which is the key attribute for comparison
+	private final int points;
 
+	// Constructor to initialize the User object with the provided points value
 	public User(int points) {
-		this.points = points;
+		this.points = points; // Assigns the passed points value to the `points` field
 	}
 
 	@Override
-	public int compareTo(User otherUser) {
-		/// Compare this user's points with another user's points.
-		/// Subtract the other user's points from this user's points:
-		/// - If the result is positive, this user has more points.
-		/// - If the result is negative, this user has fewer points.
-		/// - If the result is zero, both users have the same number of points.
-		return this.points - otherUser.points;
+	public int compareTo(@NotNull User other) {
+		/* @formatter:off
+		 * The compareTo method is overridden from the Comparable<User> interface.
+		 * This method defines how two User objects are compared based on their `points`.
+		 *
+		 * Important Concepts:
+		 * 1. Inheritance and Interface Implementation:
+		 *    - User class implements Comparable<User>, meaning User objects can be compared
+		 *    - The compareTo method enables this comparison by providing logic for it
+		 *
+		 * 2. Method Annotation:
+		 *    - @Override indicates that compareTo method overrides a superclass method
+		 *    - @NotNull from org.jetbrains.annotations ensures `other` is not null
+		 *
+		 * Comparison Logic:
+		 * 1. Returns:
+		 *    - A negative integer if `this.points` < `other.points`
+		 *       (i.e., this User has fewer points than the other User)
+		 *    - Zero if `this.points` == `other.points`
+		 *       (i.e., both Users have equal points)
+		 *    - A positive integer if `this.points` > `other.points`
+		 *       (i.e., this User has more points than the other User)
+		 *
+		 * 2. The primary criterion for comparison between User objects is
+		 *    the difference in their `points`:
+		 *    - The subtraction `this.points - other.points` determines the result
+		 * @formatter:on
+		 */
+		return this.points - other.points; // Compare points to determine the order
 	}
 
 }
--- src/part_03/_03_Generics/_8_Comparable_Interface/_8_Comparable_Interface.java
+++ src/part_03/_03_Generics/_8_Comparable_Interface/_8_Comparable_Interface.java
@@ -5,14 +5,14 @@ public class _8_Comparable_Interface {
 	public static void main(String[] args) {
 		var user1 = new User(10);
 		var user2 = new User(20);
-		if (user1.compareTo(user2) > 0) {
+		if (user1.compareTo(user2) < 0) {
 			System.out.println("user1 < user2");
 		}
 		else if (user1.compareTo(user2) == 0) {
 			System.out.println("user1 == user2");
 		}
 		else {
-			System.out.println("user1 >= user2");
+			System.out.println("user1 > user2");
 		}
 	}
 
