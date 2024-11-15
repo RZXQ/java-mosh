249a40d3f9d8d1373ecc42b01d4125b0756357e9
Reacher <macryze@icloud.com>
2024-11-16 01:44:16 +0800

    Add variable capture demo in LambdasDemo class
    Introduce a new class to demonstrate variable capture in lambda expressions and anonymous inner classes. Removed default method printTwice from Printer interface to simplify the example.

--- src/part_03/_05_Lambda_Expressions_and_Functional_Interfaces/_5_Variable_Capture/LambdasDemo.java
+++ src/part_03/_05_Lambda_Expressions_and_Functional_Interfaces/_5_Variable_Capture/LambdasDemo.java
@@ -1,25 +1,52 @@
 package part_03._05_Lambda_Expressions_and_Functional_Interfaces._5_Variable_Capture;
 
+// This class demonstrates variable capture in lambda expressions and anonymous inner classes.
+// Variable capture refers to the ability of a lambda expression or inner class to access variables
+// from its enclosing scope (method or class).
 public class LambdasDemo {
 
-	public static String prefix1 = "-";
+	// Static field in the enclosing class
+	public static String prefix2 = "-";
 
-	public String prefix2 = "-";
+	// Instance field in the enclosing class
+	public String prefix3 = "-";
 
+	// Instance method to demonstrate variable capture
+
+	// Static method that accepts a Printer functional interface
 	public static void greet(Printer printer) {
 		printer.print("Hello World");
 	}
 
 	public void show() {
+		// 1. Lambda expressions can access local variables in the enclosing method.
+		String prefix1 = "-";
 		greet(message -> System.out.println(prefix1 + message));
 
+		// 2. Lambda expressions can access static fields in the enclosing class.
 		greet(message -> System.out.println(prefix2 + message));
 
-		// this keyword represents the current instance of LambdasDemo class
-		greet(message -> System.out.println(this.prefix2));
-
-		String prefix3 = "-";
+		// 3. Lambda expressions can access instance fields in the enclosing class.
 		greet(message -> System.out.println(prefix3 + message));
+
+		// 4. In lambda expressions, 'this' refers to the instance of the enclosing class
+		// (LambdasDemo).
+		greet(message -> System.out.println(this));
+
+		// 5. 'this'
+		// - In anonymous inner classes, 'this' refers to the instance of the anonymous
+		// inner class.
+		// - Anonymous inner classes can have their own state (instance variables).
+		greet(new Printer() {
+			// Anonymous inner class can have instance variables (state).
+			private String name;
+
+			@Override
+			public void print(String message) {
+				// 'this' refers to the anonymous inner class instance.
+				System.out.println(this);
+			}
+		});
 	}
 
 }
---
+++ src/part_03/_05_Lambda_Expressions_and_Functional_Interfaces/_5_Variable_Capture/_5_Variable_Capture.java
@@ -0,0 +1,9 @@
+package part_03._05_Lambda_Expressions_and_Functional_Interfaces._5_Variable_Capture;
+
+public class _5_Variable_Capture {
+
+	public static void main(String[] args) {
+		new LambdasDemo().show();
+	}
+
+}
