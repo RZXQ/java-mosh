5ed32b777133835459e192fc3a5862781659bfb6
Reacher <macryze@icloud.com>
2024-11-26 15:47:46 +0800

    Refactor comments to clarify distinct() usage
    Updated the comments in StreamsDemo.java to clearly explain that the distinct() method is used for getting unique elements from a list. This improves code readability and helps future maintainers understand the purpose of the stream operations.

--- src/part_03/_06_Streams/_8_Getting_Unique_Elements/StreamsDemo.java
+++ src/part_03/_06_Streams/_8_Getting_Unique_Elements/StreamsDemo.java
@@ -2,12 +2,14 @@ package part_03._06_Streams._8_Getting_Unique_Elements;
 
 import java.util.List;
 
+/// Demonstrates using streams to get unique elements from a list
+/// using the distinct() method.
 public class StreamsDemo {
 
 	public static void show() {
 		var movies = List.of(new Movie("a", 10), new Movie("b", 10), new Movie("c", 20), new Movie("d", 30));
 
-		// distinct() method:
+		// 1. distinct(): get unique likes
 		movies.stream().map(Movie::getLikes).distinct().forEach(System.out::println);
 	}
 
