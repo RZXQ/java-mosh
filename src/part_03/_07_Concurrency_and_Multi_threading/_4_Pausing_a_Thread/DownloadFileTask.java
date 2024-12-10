d234a04b12e7f6ec88a9341c6cffeaf347601e63
Reacher <macryze@icloud.com>
2024-12-11 00:38:51 +0800

    Refactor concurrency-related comments and code formatting.
    Streamlined and condensed comments to focus on essential thread behavior while improving code readability through consistent formatting. Enhanced exception handling by replacing `printStackTrace` with `RuntimeException` for clearer error propagation.

--- src/part_03/_07_Concurrency_and_Multi_threading/_4_Pausing_a_Thread/DownloadFileTask.java
+++ src/part_03/_07_Concurrency_and_Multi_threading/_4_Pausing_a_Thread/DownloadFileTask.java
@@ -1,17 +1,19 @@
 package part_03._07_Concurrency_and_Multi_threading._4_Pausing_a_Thread;
 
-public class DownloadFileTask implements Runnable { // Runnable interface: represents a task to be run on a thread
-    @Override
-    public void run() {
-        System.out.println("Downloading a file: " + Thread.currentThread().getName());
+public class DownloadFileTask implements Runnable {
 
-        try {
-            // Thread.sleep(): pause the thread for 5 seconds to simulate a download delay
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void run() {
+		System.out.println("Downloading a file: " + Thread.currentThread().getName());
 
-        System.out.println("Download complete: " + Thread.currentThread().getName());
-    }
+		try {
+			Thread.sleep(5000);
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+		System.out.println("Download complete: " + Thread.currentThread().getName());
+	}
+
 }
