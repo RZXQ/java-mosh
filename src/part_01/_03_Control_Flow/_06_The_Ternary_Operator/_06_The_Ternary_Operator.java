61ed88bd32f39b4e5e73b23892fd7c3c15492ec6
Reacher <macryze@icloud.com>
2024-12-14 20:50:39 +0800

    "Fix inconsistent indentation in ternary operator example"
    Adjusted the indentation in the `main` method to ensure consistent formatting and improve code readability. No functional changes were made.

--- src/part_01/_03_Control_Flow/_06_The_Ternary_Operator/_06_The_Ternary_Operator.java
+++ src/part_01/_03_Control_Flow/_06_The_Ternary_Operator/_06_The_Ternary_Operator.java
@@ -1,13 +1,13 @@
 package part_01._03_Control_Flow._06_The_Ternary_Operator;
 
 public class _06_The_Ternary_Operator {
-    
-    public static void main(String[] args) {
-        int income = 120_000;
-        
-        // ternary operator:
-        String className = income > 100_000 ? "First" : "Economy";
-        System.out.println(className);
-    }
-    
+
+	public static void main(String[] args) {
+		int income = 120_000;
+
+		// ternary operator:
+		String className = income > 100_000 ? "First" : "Economy";
+		System.out.println(className);
+	}
+
 }
