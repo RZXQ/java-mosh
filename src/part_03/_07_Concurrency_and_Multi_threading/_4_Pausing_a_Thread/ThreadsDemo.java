8ee000d4ceaebaeb6d6d23ba428ea8a547980eef
reacher <macryze@icloud.com>
2024-12-18 20:54:09 +0800

    Set threads as daemon and enhance logging in ThreadsDemo
    Updated ThreadsDemo to mark child threads as daemon, ensuring JVM terminates once the main thread completes. Additionally, added logging to indicate when the main thread starts and finishes execution for better traceability.

--- src/part_03/_07_Concurrency_and_Multi_threading/_4_Pausing_a_Thread/ThreadsDemo.java
+++ src/part_03/_07_Concurrency_and_Multi_threading/_4_Pausing_a_Thread/ThreadsDemo.java
@@ -5,12 +5,18 @@ package part_03._07_Concurrency_and_Multi_threading._4_Pausing_a_Thread;
 public class ThreadsDemo {
 
 	public static void show() {
-		System.out.println(Thread.currentThread().getName());
+		System.out.println(Thread.currentThread().getName() + " started");
 
 		for (int i = 0; i < 10; i++) {
 			Thread thread = new Thread(new DownloadFileTask());
+
+			// The child threads will be marked as daemon threads.
+			// Once the main thread finishes, the JVM will terminate immediately, even if
+			// the daemon threads are still running.
+			thread.setDaemon(true);
 			thread.start();
 		}
+		System.out.println(Thread.currentThread().getName() + " finished");
 	}
 
 }
