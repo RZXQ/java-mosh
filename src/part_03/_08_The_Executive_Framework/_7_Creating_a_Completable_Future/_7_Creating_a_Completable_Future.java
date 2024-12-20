fee4028f87f415f1675175998ee52bba05ef46f5
Reacher <macryze@icloud.com>
2024-12-20 15:49:45 +0800

    Add exception declarations in the main method signature
    Updated the main method to declare ExecutionException and InterruptedException. This ensures proper handling of exceptions potentially thrown by CompletableFuturesDemo.show().

--- src/part_03/_08_The_Executive_Framework/_7_Creating_a_Completable_Future/_7_Creating_a_Completable_Future.java
+++ src/part_03/_08_The_Executive_Framework/_7_Creating_a_Completable_Future/_7_Creating_a_Completable_Future.java
@@ -1,8 +1,10 @@
 package part_03._08_The_Executive_Framework._7_Creating_a_Completable_Future;
 
+import java.util.concurrent.ExecutionException;
+
 public class _7_Creating_a_Completable_Future {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 		CompletableFuturesDemo.show();
 	}
