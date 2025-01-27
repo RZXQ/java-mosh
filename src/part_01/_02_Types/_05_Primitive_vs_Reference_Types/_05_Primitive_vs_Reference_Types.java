100a4d554690edf5caabbd5adec2c31bc2311581
Reacher <macryze@icloud.com>
2025-01-28 01:09:15 +0800

    Refactor comments and streamline code output formatting.
    Improved comment structure for better readability by adding section headers. Simplified output formatting by removing redundant notes, enhancing code clarity.

--- src/part_01/_02_Types/_05_Primitive_vs_Reference_Types/_05_Primitive_vs_Reference_Types.java
+++ src/part_01/_02_Types/_05_Primitive_vs_Reference_Types/_05_Primitive_vs_Reference_Types.java
@@ -5,21 +5,21 @@ import java.awt.*;
 public class _05_Primitive_vs_Reference_Types {
 
 	public static void main(String[] args) {
-		// 1. Primitive Type:
+
+		/*-------------------------------- Primitive Type --------------------------------*/
 		byte x = 1;
 		byte y = x;
 
 		x = 2;
-		System.out.println(x); // Output will be 2
-		System.out.println(y); // Output will be 1
+		System.out.println(x);
+		System.out.println(y);
 
-		// 2. Reference Type:
+		/*------------------------------- Reference Type -------------------------------*/
 		Point point1 = new Point(1, 1);
 		Point point2 = point1;
 		point1.x = 2;
 
-		System.out.println(point2); // Output will be (2, 1)
-
+		System.out.println(point2);
 	}
 
 }
