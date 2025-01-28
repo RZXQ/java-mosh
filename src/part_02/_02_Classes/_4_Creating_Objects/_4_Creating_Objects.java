eea090b82158b28b9fc0269968dbeefe6a43a114
Reacher <macryze@icloud.com>
2025-01-28 17:38:35 +0800

    Remove unused object and commented-out code in main method
    Cleaned up redundant code by removing an unused object and outdated comments related to a NullPointerException. This improves readability and eliminates unnecessary clutter in the method.

--- src/part_02/_02_Classes/_4_Creating_Objects/_4_Creating_Objects.java
+++ src/part_02/_02_Classes/_4_Creating_Objects/_4_Creating_Objects.java
@@ -6,11 +6,6 @@ public class _4_Creating_Objects {
 
 	public static void main(String[] args) {
 		TextBox textBox1 = new TextBox();
-		// System.out.println(textBox1.text.toUpperCase()); // NullPointerException
-
-		TextBox textBox2 = new TextBox();
-		textBox2.setText("Box 2");
-		System.out.println(textBox2.text);
 	}
 
 }
